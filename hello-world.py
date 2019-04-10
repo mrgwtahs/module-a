ca2cce8d850ee40e24d9a109456d7748f726405d
mrgwtahs <49483251+mrgwtahs@users.noreply.github.com>
2019-04-10 12:40:36 -0400

    Adding hello-world.py

---
+++ hello-world.py
@@ -0,0 +1,12 @@
+# Mr. Grimsley
+# 04/10/2019
+
+#####################
+#    Hello World    #
+#                   #
+#  Our First Python #
+#    Application    #
+#####################
+
+# print "Hello, World" to the screen.
+print("Hello, World")
